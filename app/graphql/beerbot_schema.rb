c584779e89dc12e34ed46e45eaa3d4b7b48bdd4a
Rene Weteling <rene@weteling.com>
2018-05-23 13:33:29 +0200

    Upped ruby version, added the order mutator

--- app/graphql/beerbot_schema.rb
+++ app/graphql/beerbot_schema.rb
@@ -1,4 +1,4 @@
 BeerbotSchema = GraphQL::Schema.define do
-  # mutation(Types::MutationType)
+  mutation(Types::MutationType)
   query(Types::QueryType)
 end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,11 +1,18 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
-  # TODO: Remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
+  field :create_order, Types::OrderType do
+    description "Create a new order"
+    argument :amount, !types.Int
+    argument :user_id, !types.Int
+    argument :item_id, !types.Int
+
     resolve ->(obj, args, ctx) {
-      "Hello World!"
+      Order.create(
+        amount: args[:amount],
+        item: Item.find(args[:item_id]),
+        user: User.find(args[:user_id]),
+      )
     }
   end
 end
